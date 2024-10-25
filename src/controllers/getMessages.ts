a8a60c35585e8189765dad33d2abc228650222d5
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-10-25 01:18:30 -0600

    UR: Se quito la paginacion de mensajes en el controlador de get messages por el momento

--- src/controllers/getMessages.ts
+++ src/controllers/getMessages.ts
@@ -4,10 +4,6 @@ import { db } from '../lib/db';
 export const getMessages = async (req: Request, res: Response): Promise<void> => {
   try {
     const { chatId } = req.params;
-    const page = parseInt(req.query.page as string) || 1;
-    const limit = parseInt(req.query.limit as string) || 10; 
-
-    const skip = (page - 1) * limit;
 
     // Buscar el chat y los detalles de la habitación
     const chat = await db.chat.findUnique({ 
@@ -19,16 +15,10 @@ export const getMessages = async (req: Request, res: Response): Promise<void> =>
       return;
     }
 
-    // Buscar los mensajes del chat
+    // Buscar todos los mensajes del chat sin paginación
     const messages = await db.message.findMany({
       where: { chatId },
       orderBy: { timestamp: 'asc' },
-      skip: skip,
-      take: limit,  
-    });
-
-    const totalMessages = await db.message.count({
-      where: { chatId },
     });
 
     // Respuesta con los detalles completos del chat y la habitación
@@ -41,11 +31,6 @@ export const getMessages = async (req: Request, res: Response): Promise<void> =>
         status: chat.status,
       },
       messages,
-      pagination: {
-        totalMessages,
-        currentPage: page,
-        totalPages: Math.ceil(totalMessages / limit),
-      },
     });
   } catch (error) {
     console.error('Error al obtener los mensajes:', error);
