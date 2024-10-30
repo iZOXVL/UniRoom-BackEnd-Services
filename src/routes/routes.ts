35f6a72d1bbdc21f7f1daa6296cd753514ccadc8
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-10-30 11:51:17 -0600

    UR: Se agregó el nuevo controlador de requestByUser v2

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import { createChat } from '../controllers/createChat';
 import { getChatsByUser } from '../controllers/chatsByUser';
 import { getMessages } from '../controllers/getMessages';
 import { updateChatStatus } from '../controllers/updateChatStatus';
+import { getRequestByUser } from '../controllers/requestByUser';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.post('/create-chat', createChat);
 router.get('/chats/:token', getChatsByUser);
 router.get('/messages/:chatId', getMessages);
 router.post('/update-status', updateChatStatus);
+router.post('/chats-request', getRequestByUser);
 
 export default router;
