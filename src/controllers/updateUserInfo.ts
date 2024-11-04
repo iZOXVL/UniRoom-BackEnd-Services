010750d5a62c3711fddeb12a1ffffcad00f4e4a0
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-11-04 15:51:47 -0600

    UR: Se añadio el modulo de eliminar cuenta v6

--- src/controllers/updateUserInfo.ts
+++ src/controllers/updateUserInfo.ts
@@ -5,27 +5,6 @@ export const updateUserInfo = async (req: Request, res: Response): Promise<void>
   try {
     const { userId, newName } = req.body;
 
-    // Verificar si existe al menos un chat o mensaje con ese userId
-    const chatExists = await db.chat.findFirst({
-      where: {
-        participants: {
-          has: userId,
-        },
-      },
-    });
-
-    const messageExists = await db.message.findFirst({
-      where: {
-        senderId: userId,
-      },
-    });
-
-    // Si no existe ni en chats ni en mensajes, devolver error
-    if (!chatExists && !messageExists) {
-      res.status(404).json({ status: 'error', message: 'Usuario no encontrado en chats o mensajes.' });
-      return;
-    }
-
     // Actualizar el nombre del usuario en `participantDetails` dentro de los chats donde el usuario es participante
     const chats = await db.chat.findMany({
       where: {
@@ -36,18 +15,18 @@ export const updateUserInfo = async (req: Request, res: Response): Promise<void>
     });
 
     for (const chat of chats) {
-        const updatedParticipantDetails = Array.isArray(chat.participantDetails)
-          ? chat.participantDetails.map((participant: any) =>
-              participant.id === userId ? { ...participant, name: newName } : participant
-            )
-          : [];
-        await db.chat.update({
-          where: { id: chat.id },
-          data: {
-            participantDetails: updatedParticipantDetails,
-          },
-        });
-      }
+      const updatedParticipantDetails = Array.isArray(chat.participantDetails)
+        ? chat.participantDetails.map((participant: any) =>
+            participant.id === userId ? { ...participant, name: newName } : participant
+          )
+        : [];
+      await db.chat.update({
+        where: { id: chat.id },
+        data: {
+          participantDetails: updatedParticipantDetails,
+        },
+      });
+    }
 
     // Actualizar el nombre del usuario en la colección de mensajes
     const updatedMessages = await db.message.updateMany({
@@ -65,6 +44,6 @@ export const updateUserInfo = async (req: Request, res: Response): Promise<void>
     });
   } catch (error) {
     console.error('Error al actualizar el nombre del usuario en chats y mensajes:', error);
-    res.status(500).json({ status: 'error', message: 'Error al actualizar el nombre del usuario en chats y mensajes.' });
+    res.status(202).json({ status: 'error', message: 'Error al actualizar el nombre del usuario en chats y mensajes.' });
   }
 };
