7ebaa5b3376500b6eefa318e9766949566045e7d
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-11-08 11:14:07 -0600

    UR: Se implemento correctamente el modulo para saber si el servicio esta disponible v2

--- src/controllers/status/serviceAlive.ts
+++ src/controllers/status/serviceAlive.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 
 export const serviceAlive = async (req: Request, res: Response): Promise<void> => {
-    const service = true;
-    if(service){
+    const service = process.env.SERVICE_ALIVE as string;    
+    if(service==='true'){
         res.status(200).json({ service: true, message: 'El servicio está habilitado' });
     }else{
         res.status(503).json({ service: false, message: 'El servicio está fuera de servicio' });
